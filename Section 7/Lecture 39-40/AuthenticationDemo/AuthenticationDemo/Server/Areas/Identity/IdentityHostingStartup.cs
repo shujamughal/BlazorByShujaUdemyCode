82ae819388641ba4a019cdecf1cae1311ddf0ded
shujamughal <shujamughal@gmail.com>
2021-04-14 16:04:03 +0500

    section 7

---
+++ Section 7/Lecture 39-40/AuthenticationDemo/AuthenticationDemo/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AuthenticationDemo.Server.Data;
+using AuthenticationDemo.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticationDemo.Server.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticationDemo.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
