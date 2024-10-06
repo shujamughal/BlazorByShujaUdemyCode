bee10c76efca982af6b5660aa60396317ed1f280
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-10-06 13:03:04 +0500

    added templated component example

---
+++ blazor-net8/BlazorEcommerceExamples/BWA_TemplatedComponentExample/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BWA_TemplatedComponentExample.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
