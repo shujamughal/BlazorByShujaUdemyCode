006ae9a8011cbb99cc0139428198a6f4536ff68c
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-10-27 15:52:48 +0500

    PWA example added with localstorage

---
+++ blazor-net8/BWA_PWA_Example/wwwroot/js/localStorage.js
@@ -0,0 +1,10 @@
+﻿// localStorageHelper.js
+window.localStorageHelper = {
+    loadNotes: function () {
+        const savedNotes = localStorage.getItem("notes");
+        return savedNotes ? savedNotes : JSON.stringify([]); // Return empty array if no notes
+    },
+    saveNotes: function (notes) {
+        localStorage.setItem("notes", notes);
+    }
+};
